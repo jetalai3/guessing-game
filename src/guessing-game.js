1cdfbedd7e7b913ed34695e4820cbfa96c367f55
Viktar Svintsitski <jetalai3@gmail.com>
2019-10-01 01:19:13 +0300

    feat: add game class implementation

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.result = 0;
+        this.setRange = this.setRange.bind(this);
+        this.guess = this.guess.bind(this);
+        this.lower = this.lower.bind(this);
+        this.greater = this.greater.bind(this);
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.result = Math.ceil((this.max - this.min) / 2);
     }
 
     guess() {
-
+        return this.result;
     }
 
     lower() {
-
+        this.max = this.result;
+        this.result = Math.ceil((this.max - this.min) / 2 + this.min);
     }
 
     greater() {
-
+        this.min = this.result;
+        this.result = Math.ceil((this.max - this.min) / 2 + this.min);
     }
 }
 
